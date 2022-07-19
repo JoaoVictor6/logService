9b4999e8a853de001f8f3a3b0ea377e429044ef7
João Victor <santosjoao.dev@gmail.com>
2022-07-19 12:18:58 -0300

    refactor: improve log message

--- src/logger.js
+++ src/logger.js
@@ -4,5 +4,10 @@ morgan.token('userId', (req, res) => {
   return req.id || 'no-auth'
 })
 
+morgan.token('body', (req, res) => {
+  const body = JSON.stringify(req.body)   
+  return body === "{}" ? 'no-body' : body
+})
 
-export const logger = morgan('LOG > :userId :method :status :url :remote-addr :date[iso]')
+
+export const logger = morgan('LOG > :userId :method :body :status :url :remote-addr :date[iso]')
